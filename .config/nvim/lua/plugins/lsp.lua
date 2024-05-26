99f9f1b6c98f11268ddf8fb37facc865ccead4c7
Mauritz <mauritz.sjodin3@gmail.com>
2024-05-27 01:53:55 +0200

    nvim show signature help with ctrl+s

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
         vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
         vim.keymap.set("n", "<C-w>d", vim.diagnostic.open_float, opts)
-        vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, opts)
         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
     end,
