ad5f6325d55eaa6097da4f29690c1c022745f1d4
Mauritz <mauritz.sjodin3@gmail.com>
2024-06-15 10:35:22 +0200

    use gitsigns for git exclusively, instead of fugitive

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,20 +1,42 @@
 return {
-    {
-        'lewis6991/gitsigns.nvim',
-        config = function()
-            require('gitsigns').setup()
-        end
-    },
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup({
+        on_attach = function(bufnr)
+          local gitsigns = require('gitsigns')
 
-    {
-        'tpope/vim-fugitive',
-        config = function()
-            -- choose fix when resolving merge conflict
-            -- open file containing the merge conflict in a three-way split with the shortcut "dv"
-            -- then use either ml for "merge left", or mr for "merge right"
-            -- when you're done press dq to close the split 
-            vim.keymap.set("n", "<leader>ml", "<cmd>diffget //2<CR>")
-            vim.keymap.set("n", "<leader>mr", "<cmd>diffget //3<CR>")
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map('n', ']c', function()
+            if vim.wo.diff then
+              vim.cmd.normal({ ']c', bang = true })
+            else
+              gitsigns.nav_hunk('next')
+            end
+          end)
+
+          map('n', '[c', function()
+            if vim.wo.diff then
+              vim.cmd.normal({ '[c', bang = true })
+            else
+              gitsigns.nav_hunk('prev')
+            end
+          end)
+
+          -- Actions
+          map('n', '<leader>hp', gitsigns.preview_hunk)
+          map('n', '<leader>hs', gitsigns.stage_hunk)
+          map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+          map('n', '<leader>hr', gitsigns.reset_hunk)
+          map('n', '<leader>hd', gitsigns.diffthis)
         end
-    }
+      })
+    end
+  },
 }
