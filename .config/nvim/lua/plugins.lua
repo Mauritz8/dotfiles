949c18bd079d326d55e382a77c98961fe486c647
Mauritz Sjödin <mauritz.sjodin@protonmail.com>
2025-12-05 00:24:13 +0100

    feat: install lualine and overhaul tmux config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -152,5 +152,26 @@ return {
     'nvim-flutter/flutter-tools.nvim',
     lazy = false,
     opts = {},
+  },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup({
+        options = {
+            component_separators = { left = '', right = '|'},
+            section_separators = { left = '', right = ''},
+          },
+          sections = {
+            lualine_a = {'mode'},
+            lualine_b = {'branch'},
+            lualine_c = {'filename'},
+            lualine_x = {'encoding', 'fileformat', 'filetype'},
+            lualine_y = {'progress'},
+            lualine_z = {'location'}
+          },
+      })
+    end
   }
 }
